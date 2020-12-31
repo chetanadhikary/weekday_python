f85045a06e89942417b7d5aa6bb4a98f647dfe67
chetanadhikary <chetanadhikary@gmail.com>
2020-12-31 12:10:52 +0530

    Day 8 updated

--- looping_statements.py
+++ looping_statements.py
@@ -49,29 +49,56 @@ x = 10
 ##        print(f'even list:{lst_even}\nodd list:{lst_odd}')
 ##        break
 
-
-
-lst_numbers = [1,2,3,4,5,6,23,239]
-lst_sq = []
-i = 0
-while i < len(lst_numbers):
-    sq_num = lst_numbers[i]**2
-    lst_sq.append(sq_num)
-    i += 1
-else:
-    print(f'Using while loop:{lst_sq}')
-
-lst_sq_f = []
-for num in lst_numbers:
-    lst_sq_f.append(num**2)
-else:
-    print(f'Using for loop:{lst_sq_f}')
-
+##
+##
+##lst_numbers = [1,2,3,4,5,6,23,239]
+##lst_sq = []
+##i = 0
+##while i < len(lst_numbers):
+##    sq_num = lst_numbers[i]**2
+##    lst_sq.append(sq_num)
+##    i += 1
+##else:
+##    print(f'Using while loop:{lst_sq}')
+##
+##lst_sq_f = []
+##for num in lst_numbers:
+##    lst_sq_f.append(num**2)
+##else:
+##    print(f'Using for loop:{lst_sq_f}')
+##
 
 
 ## Write a program which will accept the user number and add to a list 
 ## If user inputs an alphabet/string the program should exit printing the list
 ## Calculate the sum of the numbers,maximum of the numbers, minimum of the numbers
+
+
+
+
+### Write a program to identify the datatypes in the list .
+
+
+## Write a program to add items to a key
+
+##dict_cat_1 = {'fruits':'banana','colours':'pink','animals':'dog','tech':'c'}
+##dict_cat_2 = {'fruits':'apple','colours':'red','animals':'lion','tech':'python'}
+##dict_cat_master = {'fruits':[],'colours':[],'animals':[],'tech':[]}
+
+sentence = 'She sells sea shells on the sea shore'
+lst_words = sentence.split(' ')
+dict_words = dict()
+for word in lst_words:
+    dict_words[word] = dict_words.get(word,0) + 1
+##    if word in dict_words.keys():
+##        dict_words[word] = dict_words[word] + 1
+##        print(dict_words[word])
+##    else:
+##        dict_words[word] = 1
+##        print(f'{word} not in dictionary')
+    print(dict_words)
+
+
 
 
 
